cb84133c4a04f33461a25f912c353039cda88188
The Beardless <ABeardless@gmx.net>
2024-10-10 10:32:23 +0200

    Added changing text

---
+++ content/js/index.js
@@ -0,0 +1,16 @@
+{
+  const words = ["GAME-DEVELOPMENT", "MUSIC", "3D ART", "WEB-DESIGN", "SOFTWARE-DEVELOPMENT"];
+  let currentIndex = 0;
+
+  function changeText() {
+    const changingTextElements = document.getElementsByClassName("changing-text");
+
+    Array.from(changingTextElements).forEach(function(element) {
+      element.textContent = " // " + words[currentIndex];
+    });
+
+    currentIndex = (currentIndex + 1) % words.length;
+  }
+
+  setInterval(changeText, 4000);
+}
